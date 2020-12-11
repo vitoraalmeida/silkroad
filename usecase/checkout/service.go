58bdc85bd08a767a5b243453ed358e1a4510c0b3
Vitor Almeida <pro.vitoralmeida@gmail.com>
2020-12-11 11:49:32 -0300

    Add validations to checout usecase

--- usecase/checkout/service.go
+++ usecase/checkout/service.go
@@ -3,11 +3,16 @@ package checkout
 import (
 	"errors"
 	"fmt"
+
+	"github.com/vitoraalmeida/silkroad/usecase/customer"
+	"github.com/vitoraalmeida/silkroad/usecase/product"
 	"github.com/vitoraalmeida/silkroad/usecase/sale"
 	"github.com/vitoraalmeida/silkroad/usecase/saleitem"
 )
 
 type Service struct {
+	customerService customer.UseCase
+	productService  product.UseCase
 	saleService     sale.UseCase
 	saleItemService saleitem.UseCase
 }
@@ -22,34 +27,57 @@ type Cart []CartItem
 
 var ErrInvalidCustomer = errors.New("Customer doesn't exist")
 var ErrCartEmpty = errors.New("Cart cannot be empty")
+var ErrInvalidProduct = errors.New("Product doesn't exist")
 
-func NewService(s sale.UseCase, si saleitem.UseCase) *Service {
+func NewService(s sale.UseCase, si saleitem.UseCase, cs customer.UseCase, ps product.UseCase) *Service {
 	return &Service{
+		customerService: cs,
+		productService:  ps,
 		saleService:     s,
 		saleItemService: si,
 	}
 }
 
 func (s *Service) Checkout(c *Cart, customerID uint) error {
-	if len(*c) == 0 {
-		return ErrCartEmpty
-	}
-	if customerID <= 0 {
+	_, err := s.customerService.GetCustomer(customerID)
+	if err != nil {
 		return ErrInvalidCustomer
 	}
 
+	// check cart
+	if len(*c) == 0 {
+		return ErrCartEmpty
+	}
+
+	for _, ci := range *c {
+		p, err := s.productService.GetProduct(ci.ProductID)
+		if err != nil {
+			return ErrInvalidProduct
+		}
+		if p.Stock < ci.Quantity {
+			return fmt.Errorf("Item Cart: %#v\nProduct %d out of stock", ci, ci.ProductID)
+		}
+	}
+
+	// calculate cart's total amount
 	totalAmount := 0.00
 	for _, ci := range *c {
 		totalAmount += ci.Subtotal
 	}
 
+	// create sale
 	saleID, _ := s.saleService.CreateSale(customerID, totalAmount)
 
-	fmt.Println("\nSale ID: ", saleID)
-
+	// register sale's items
 	for _, ci := range *c {
-		id, _ := s.saleItemService.CreateSaleItem(saleID, ci.ProductID, ci.Quantity, ci.Subtotal)
-		fmt.Println(id)
+		err := s.productService.DecrementProductStock(ci.ProductID, ci.Quantity)
+		if err != nil {
+			return fmt.Errorf("Item Cart: %#v\nProduct %d out of stock", ci, ci.ProductID)
+		}
+		_, err = s.saleItemService.CreateSaleItem(saleID, ci.ProductID, ci.Quantity, ci.Subtotal)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
