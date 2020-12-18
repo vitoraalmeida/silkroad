7a40c9f2765ef5f8b2daaec2f531b6c52154dc60
Vitor Almeida <pro.vitoralmeida@gmail.com>
2020-12-17 21:32:42 -0300

    Add http server and gorilla router

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/vitoraalmeida/silkroad
 go 1.15
 
 require (
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.9.0
 )
--- main.go
+++ main.go
@@ -6,27 +6,34 @@ import (
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"log"
+	"net/http"
 	"os"
-	"time"
+	_ "time"
 
-	"github.com/vitoraalmeida/silkroad/entity"
-	"github.com/vitoraalmeida/silkroad/infra/repository"
-	"github.com/vitoraalmeida/silkroad/usecase/category"
-	"github.com/vitoraalmeida/silkroad/usecase/checkout"
-	"github.com/vitoraalmeida/silkroad/usecase/customer"
-	"github.com/vitoraalmeida/silkroad/usecase/delivery"
-	"github.com/vitoraalmeida/silkroad/usecase/product"
-	"github.com/vitoraalmeida/silkroad/usecase/sale"
-	"github.com/vitoraalmeida/silkroad/usecase/saleitem"
+	"github.com/gorilla/mux"
+
+	_ "github.com/vitoraalmeida/silkroad/entity"
+	_ "github.com/vitoraalmeida/silkroad/infra/repository"
+	_ "github.com/vitoraalmeida/silkroad/usecase/category"
+	_ "github.com/vitoraalmeida/silkroad/usecase/checkout"
+	_ "github.com/vitoraalmeida/silkroad/usecase/customer"
+	_ "github.com/vitoraalmeida/silkroad/usecase/delivery"
+	_ "github.com/vitoraalmeida/silkroad/usecase/product"
+	_ "github.com/vitoraalmeida/silkroad/usecase/sale"
+	_ "github.com/vitoraalmeida/silkroad/usecase/saleitem"
 )
 
-type CartItem struct {
-	ProductID uint
-	Quantity  uint
-	Subtotal  float64
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
-type Cart []CartItem
+func admin(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email "+
+		"to <a href=\"mailto:support@lenslocked.com\">"+
+		"support@lenslocked.com</a>.")
+}
 
 func main() {
 
@@ -48,362 +55,29 @@ func main() {
 	}
 	defer db.Close()
 
-	// category
-	cr := repository.NewCategoryPQSL(db)
-	cs := category.NewService(cr)
-	// product
-	pr := repository.NewProductPQSL(db)
-	ps := product.NewService(pr)
-	// sale
-	sr := repository.NewSalePQSL(db)
-	ss := sale.NewService(sr)
-	// customer
-	csr := repository.NewCustomerPQSL(db)
-	css := customer.NewService(csr)
-	// saleitem
-	sir := repository.NewSaleItemPQSL(db)
-	sis := saleitem.NewService(sir)
-	// delivery
-	dr := repository.NewDeliveryPSQL(db)
-	ds := delivery.NewService(dr)
-	// checkout
-	chs := checkout.NewService(ss, sis, css, ps, ds)
+	//// category
+	//cr := repository.NewCategoryPQSL(db)
+	//cs := category.NewService(cr)
+	//// product
+	//pr := repository.NewProductPQSL(db)
+	//ps := product.NewService(pr)
+	//// sale
+	//sr := repository.NewSalePQSL(db)
+	//ss := sale.NewService(sr)
+	//// customer
+	//csr := repository.NewCustomerPQSL(db)
+	//css := customer.NewService(csr)
+	//// saleitem
+	//sir := repository.NewSaleItemPQSL(db)
+	//sis := saleitem.NewService(sir)
+	//// delivery
+	//dr := repository.NewDeliveryPSQL(db)
+	//ds := delivery.NewService(dr)
+	//// checkout
+	//chs := checkout.NewService(ss, sis, css, ps, ds)
 
-	fmt.Println(cs.CreateCategory("Livros"))
-	fmt.Println(cs.CreateCategory("roupas"))
-	fmt.Println(cs.CreateCategory("livros"))
-	fmt.Println(cs.CreateCategory("livros capa dura"))
-	fmt.Println(cs.CreateCategory("Tipo Livros"))
-	category, err := cs.GetCategory(1)
-	if err != nil {
-		fmt.Println("Not found")
-		return
-	}
-	fmt.Printf("%v\n", category)
-
-	//fmt.Println("\n-------------------------- All categories ---------------------------------\n")
-	categories, err := cs.ListCategories()
-	if err != nil {
-		fmt.Errorf("%v", err)
-		return
-	}
-	for _, v := range categories {
-		fmt.Printf("%v\n", v)
-	}
-
-	c1, err := cs.GetCategory(1)
-	fmt.Printf("Atualizando: %+v\n", c1)
-	c1.Name = "Livro"
-	fmt.Printf("Para: %+v\n", c1)
-	err = cs.UpdateCategory(c1)
-	if err != nil {
-		fmt.Println(err)
-	}
-	c1, err = cs.GetCategory(1)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Printf("%+v\n", c1)
-
-	err = cs.DeleteCategory(1)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	categories, err = cs.ListCategories()
-	if err != nil {
-		fmt.Errorf("%v", err)
-		return
-	}
-	fmt.Println("depois de deletar")
-	for _, v := range categories {
-		fmt.Printf("%v\n", v)
-	}
-	fmt.Println("todas que contenham 'ivros'")
-	categories, err = cs.SearchCategories("livros")
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, v := range categories {
-		fmt.Printf("%v\n", v)
-	}
-
-	//// ------------------------------- Produtos -----------------------------------
-	fmt.Println("\n-------------------------- Products ---------------------------------\n")
-
-	fmt.Println(ps.CreateProduct("O Capital Volume 1", 3, 60.00, 10, true))
-	fmt.Println(ps.CreateProduct("O Capital Volume 2", 3, 70.00, 10, true))
-	fmt.Println(ps.CreateProduct("O Capital Volume 3", 3, 70.00, 50, true))
-
-	fmt.Println(ps.GetProduct(1))
-
-	fmt.Println(ps.CreateProduct("Capital Inicial M", 2, 30.00, 10, true))
-
-	//fmt.Println("\n------- Search products containing 'capital' ---------------------------------\n")
-	//capitals, _ := ps.SearchProducts("Capital")
-	//for _, p := range capitals {
-	//	fmt.Printf("%+v\n", p)
-	//}
-
-	fmt.Println("\n-------------------------- All Products ---------------------------------\n")
-	products, err := ps.ListProducts()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for _, p := range products {
-		fmt.Printf("%+v\n", p)
-	}
-
-	fmt.Println("\n-------------------------- Updating Product ------------------------------\n")
-	capital, _ := ps.GetProduct(1)
-	changedCapital := &entity.Product{
-		ID:         1,
-		Name:       "Das Kapital",
-		CategoryID: 2,
-		Price:      100.00,
-		Stock:      5,
-		Available:  true,
-		CreatedAt:  capital.CreatedAt,
-	}
-	fmt.Printf("Updating\n\n%+v\n\nto\n\n %+v\n", capital, changedCapital)
-
-	time.Sleep(5 * time.Second)
-	err = ps.UpdateProduct(changedCapital)
-	if err != nil {
-		fmt.Println("Not changed")
-	}
-
-	fmt.Println("\n-------------------------- All Products ---------------------------------\n")
-	products, _ = ps.ListProducts()
-	for _, p := range products {
-		fmt.Printf("%+v\n", p)
-	}
-
-	fmt.Println("\n-------------------------- Decrementing product --------------------------------\n")
-	fmt.Println(ps.GetProduct(1))
-	err = ps.DecrementProductStock(1, 1)
-	err = ps.DeleteProduct(2)
-
-	fmt.Println("\n-------------------------- All Products ---------------------------------\n")
-	products, _ = ps.ListProducts()
-	for _, p := range products {
-		fmt.Printf("%+v\n", p)
-	}
-
-	fmt.Println("\n\ntodos que contenham 'apital'")
-	products, err = ps.SearchProducts("apital")
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, v := range products {
-		fmt.Printf("%v\n", v)
-	}
-
-	fmt.Println("\n\n------------------------- Customers --------------------------------------\n")
-	fmt.Println(css.CreateCustomer("Vitor", "vitor@mail.com", "66666666666", "asenha"))
-	fmt.Println(css.CreateCustomer("Ana", "ana@mail.com", "77777777777", "asenha"))
-	fmt.Println(css.CreateCustomer("Fred", "fred@mail.com", "88888888888", "asenha"))
-
-	fmt.Println(css.GetCustomer(1))
-
-	fmt.Println("\n\n------------------------- All Customers --------------------------------------\n")
-	customers, _ := css.ListCustomers()
-	for _, p := range customers {
-		fmt.Printf("%+v\n", p)
-	}
-
-	fmt.Println("\n-------------------------- Updating Customer ------------------------------\n")
-	customer, _ := css.GetCustomer(1)
-	changedCustomer := &entity.Customer{
-		ID:        1,
-		Name:      "Vitor Almeida",
-		Email:     "vitor@gmail.com",
-		CPF:       "66666666666",
-		Password:  "asenha2",
-		CreatedAt: customer.CreatedAt,
-	}
-	fmt.Printf("Updating\n\n%+v\n\nto\n\n %+v\n", customer, changedCustomer)
-
-	time.Sleep(5 * time.Second)
-	err = css.UpdateCustomer(changedCustomer)
-	if err != nil {
-		fmt.Println("Not changed")
-	}
-
-	fmt.Println("\n\n------------------------- All Customers --------------------------------------\n")
-	customers, _ = css.ListCustomers()
-	for _, p := range customers {
-		fmt.Printf("%+v\n", p)
-	}
-
-	fmt.Println("\n\n------------------------- Delete Customer --------------------------------------\n")
-	err = css.DeleteCustomer(1)
-
-	fmt.Println("\n\n------------------------- All Customers --------------------------------------\n")
-	customers, _ = css.ListCustomers()
-	for _, p := range customers {
-		fmt.Printf("%+v\n", p)
-	}
-
-	//fmt.Println("\n\n------------------------- Sales --------------------------------------\n")
-	//fmt.Println(ss.CreateSale(2, 999.99))
-	//fmt.Println(ss.CreateSale(2, 750.00))
-	//fmt.Println(ss.CreateSale(3, 720.00))
-
-	//fmt.Println(ss.GetSale(1))
-
-	//fmt.Println("\n\n------------------------- All Sales --------------------------\n")
-	//sales, err := ss.ListSales()
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//for _, v := range sales {
-	//	fmt.Printf("%v\n", v)
-	//}
-
-	//fmt.Println("\n\n------------------------- Delete Sale --------------------------------------\n")
-	//err = ss.DeleteSale(1)
-
-	//fmt.Println("\n\n------------------------- Customer 2's sales --------------------------\n")
-	//sales, err = ss.SearchSales(2)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//for _, v := range sales {
-	//	fmt.Printf("%v\n", v)
-	//}
-
-	//fmt.Println("\n\n------------------------- Sale 2's items --------------------------\n")
-
-	//fmt.Println(sis.CreateSaleItem(2, 1, 4, 400.00))
-	//fmt.Println(sis.CreateSaleItem(2, 3, 5, 350.00))
-
-	//fmt.Println(sis.GetSaleItem(1))
-
-	//fmt.Println("\n\n------------------------- Sale 3's items --------------------------\n")
-
-	//fmt.Println(sis.CreateSaleItem(3, 1, 3, 300.00))
-	//fmt.Println(sis.CreateSaleItem(3, 3, 6, 420.00))
-
-	//fmt.Println("\n\n------------------------- All Sale items --------------------------\n")
-	//saleItems, err := sis.ListSaleItems()
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//for _, v := range saleItems {
-	//	fmt.Printf("%v\n", v)
-	//}
-
-	//fmt.Println("\n\n------------------------- Sale 2's items --------------------------\n")
-	//saleItems, err = sis.SearchItemsBySaleID(2)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//for _, v := range saleItems {
-	//	fmt.Printf("%v\n", v)
-	//}
-
-	fmt.Println("\n\n----------------------- Carrinho 1 -----------------------------------\n")
-
-	ci1 := checkout.CartItem{1, 3, 180.00}
-	ci2 := checkout.CartItem{3, 5, 350.00}
-	ci3 := checkout.CartItem{4, 4, 120.00}
-	cart := &checkout.Cart{ci1, ci2, ci3}
-	fmt.Printf("Cart: %+v", cart)
-
-	fmt.Println("\n\n----------------------- Checkout 1 ----------------------------------------------")
-	err = chs.Checkout(cart, 2)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println("--------------------------- Sale Items ----------------------------------- \n")
-	saleItems, _ := sis.ListSaleItems()
-	for _, i := range saleItems {
-		fmt.Printf("%+v\n", i)
-	}
-
-	fmt.Println("\n------------------------- All Sales ------------------------------------ \n")
-	sales, _ := ss.ListSales()
-	for _, s := range sales {
-		fmt.Printf("%+v\n", s)
-	}
-
-	fmt.Println("\n---------------------- Sale1's items ----------------- \n")
-	saleItems, _ = sis.SearchItemsBySaleID(1)
-	for _, si := range saleItems {
-		fmt.Printf("%+v\n", si)
-	}
-
-	fmt.Println("\n-------------------------- All Products ---------------------------------\n")
-	products, _ = ps.ListProducts()
-	for _, p := range products {
-		fmt.Printf("%+v\n", p)
-	}
-
-	fmt.Println("\n\n---------------------- Carrinho 2 -----------------------------------\n")
-
-	ci1 = checkout.CartItem{1, 1, 100.00}
-	ci2 = checkout.CartItem{3, 4, 120.00}
-	cart = &checkout.Cart{ci1, ci2}
-	fmt.Printf("Cart: %+v\n", cart)
-
-	fmt.Println(" checkout 2 ")
-	err = chs.Checkout(cart, 2)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println("\n-------------------------- All Products ---------------------------------\n")
-	products, _ = ps.ListProducts()
-	for _, p := range products {
-		fmt.Printf("%+v\n", p)
-	}
-
-	fmt.Println("\n ----------------------- All Sale Items -------------------------------- \n")
-	saleItems, _ = sis.ListSaleItems()
-	for _, i := range saleItems {
-		fmt.Printf("%+v\n", i)
-	}
-
-	fmt.Println("\n ----------------------- All Sales -------------------------------- \n")
-	sales, _ = ss.ListSales()
-	for _, s := range sales {
-		fmt.Printf("%+v\n", s)
-	}
-
-	fmt.Println("\n ----------------------- All deliveries -------------------------- \n")
-	deliveries, _ := ds.ListDeliveries()
-	for _, d := range deliveries {
-		fmt.Printf("%+v\n", d)
-	}
-
-	fmt.Println("\n ----------------------- Update delivery 1 -------------------------- \n")
-
-	delivery, _ := ds.GetDelivery(1)
-	changedDelivery := &entity.Delivery{
-		ID:         delivery.ID,
-		SaleID:     delivery.SaleID,
-		CustomerID: delivery.CustomerID,
-		Address:    "Av 1 n 29",
-		Status:     "encaminhado",
-		CreatedAt:  delivery.CreatedAt,
-	}
-	fmt.Printf("Updating\n\n%+v\n\nto\n\n %+v\n", delivery, changedDelivery)
-
-	time.Sleep(5 * time.Second)
-	err = ds.UpdateDelivery(changedDelivery)
-	if err != nil {
-		fmt.Println("Not changed")
-	}
-
-	fmt.Println("\n ----------------------- All deliveries -------------------------- \n")
-	deliveries, _ = ds.ListDeliveries()
-	for _, d := range deliveries {
-		fmt.Printf("%+v\n", d)
-	}
-
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/admin", admin)
+	http.ListenAndServe(":3000", r)
 }
